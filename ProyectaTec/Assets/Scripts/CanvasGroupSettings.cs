63d6ea35e407b34caa50f21878e7f8f7e98db3a6
unknown <a01421467@itesm.mx>
2020-04-06 21:30:19 -0500

    proyect with basic navigation

---
+++ ProyectaTec/Assets/Scripts/CanvasGroupSettings.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasGroupSettings : MonoBehaviour
+{
+    [Header("Initial settings")]
+    [SerializeField] [Range(0,1)] float alpha_float = 0;
+    [SerializeField] bool interactable_bool = false;
+    [SerializeField] bool blockRaycast_bool = false;
+    
+    private CanvasGroup cg;
+
+    protected void Awake() {
+        if (cg == null) {
+            if (GetComponent<CanvasGroup> () == null) {
+                cg = this.gameObject.AddComponent (typeof(CanvasGroup)) as CanvasGroup; 
+            } else {
+                cg = this.GetComponent<CanvasGroup> (); 
+            }
+        } 
+    }
+
+    void Start()
+    {
+        cg.alpha = alpha_float;
+        cg.interactable = interactable_bool;
+        cg.blocksRaycasts = blockRaycast_bool;
+    }
+
+    public void SetInteractable(bool interactable)
+    {
+        if (interactable) {
+            cg.alpha = 1;    
+        }else{
+            cg.alpha = 0;  
+        }
+        cg.interactable = interactable;
+        cg.blocksRaycasts = interactable;
+    }
+
+
+}
